20f00083373fb71c6cebc295b2e0a35549191b89
pabloramoss <pablo-362@hotmail.com>
2022-09-27 22:49:17 -0300

    add styled components and eslint rules

--- types/index.tsx
+++ types/index.tsx
@@ -7,7 +7,7 @@ export interface ScheduleRange {
 export type Day = LaborDay | DayOff;
 
 // LaborDay = [{start: "9:00", end: "12:00"}, {start: "16:00", end: "20:00"}]
-export type LaborDay = ScheduleRange[]
+export type LaborDay = ScheduleRange[];
 
 export type DayOff = null;
 
@@ -19,19 +19,19 @@ export type Week = {
   friday: Day;
   saturday: Day;
   sunday: Day;
-}
+};
 
 // DayHour = "9:00" | "10:00" ...
 export type DayHour = string;
 
 // AppointmentInterval =  "15 min" | "30 min" | "1 hora" | "1h 30min" | "2 horas"
-export type AppointmentInterval = .25 | .5  | 1 | 1.5 | 2;
+export type AppointmentInterval = 0.25 | 0.5 | 1 | 1.5 | 2;
 
 // Employee = {name: "John", week: {monday...}}
 export interface Employee {
   name: string;
   week: Week;
-};
+}
 
 // Stuff = [{name: "John", week: {monday...}},{name: "Doe", week: {monday...}}]
 export type Stuff = Employee[];
