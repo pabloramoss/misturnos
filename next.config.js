9b6176600f918a6abd279d941ed1f9394053c329
pabloramoss <pablo-362@hotmail.com>
2022-10-31 22:56:28 -0300

    update to nextjs 13

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compiler: {
-    styledComponents: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
